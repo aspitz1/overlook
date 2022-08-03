7fd130046915502b8f93a82e264e8ed1e7a4e966
Anna Spitz <aspitzemail@gmail.com>
2022-08-03 11:31:09 -0500

    Add sample test data

---
+++ test/test-data/Customer-data.js
@@ -0,0 +1,14 @@
+export const customer = [
+    {
+        "id": 9,
+        "name": "Faustino Quitzon"
+    },
+    {
+        "id": 12,
+        "name": "Kari Keeling"
+    },
+    {
+        "id": 27,
+        "name": "Sigrid Barrows"
+    }
+];
