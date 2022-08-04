95fb9b7e66e2e85b964e13cb2976c4ce55e685be
Anna Spitz <aspitzemail@gmail.com>
2022-08-04 17:32:04 -0500

    Add tested Manager class

--- src/classes/Manager.js
+++ src/classes/Manager.js
@@ -1,8 +1,23 @@
+import Customer from './Customer';
+
 class Manager {
-    constructor() {
+    constructor(customersAndBookings) {
         this.loginName = 'manager';
         this.password = 'overlook2021';
+        this.allCustomers = this.getAllCustomers(customersAndBookings)
     }
+
+    getAllCustomers(allCustomersAndAllBookings) {
+        return allCustomersAndAllBookings.customers
+            .map(customer => new Customer({customer: customer, allBookings: allCustomersAndAllBookings.allBookings}))
+    }
+
+    getPercentAvailibleRooms(allRoomsAllBookingsAndDate) {
+        const unavailibleRoomNumbers = allRoomsAllBookingsAndDate.allBookings.filter(booking => booking.date === allRoomsAllBookingsAndDate.date)
+            .map(booking => booking.roomNumber);
+        return `${Math.floor((unavailibleRoomNumbers.length / allRoomsAllBookingsAndDate.allRooms.length) * 100)}%`
+    }
+
 }
 
 export default Manager;
