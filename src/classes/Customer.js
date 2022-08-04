4086b63fb55117ff1cd2f1d5113f75add2b83d55
Anna Spitz <aspitzemail@gmail.com>
2022-08-03 20:10:20 -0500

    Add tested Customer class

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,7 +1,36 @@
-class Customer {
-    constructor() {
+import { bookings } from '../../test/test-data/booking-data';
 
+class Customer {
+    constructor(customerData) {
+        this.id = customerData.id;
+        this.name = customerData.name;
+        this.loginName = `customer${this.id}`;
+        this.password = 'overlook2021';
+        this.futureBookings = this.findFutureBookings(bookings);
+        this.pastBookings = this.findPastBookings(bookings);
     }
+
+    findFutureBookings(allBookings) {
+        const todaysDate = `${new Date().getFullYear()}/${new Date().getMonth()}/${new Date().getDate()}`;
+        return allBookings.filter(booking => booking.date >= todaysDate && booking.userID === this.id);
+    }
+
+    findPastBookings(allBookings) {
+        const todaysDate = `${new Date().getFullYear()}/${new Date().getMonth()}/${new Date().getDate()}`;
+        return allBookings.filter(booking => booking.date < todaysDate && booking.userID === this.id);
+    }
+
+    returnTotalSpent(allRooms) {
+        const roomNumbers = this.pastBookings.map(booking => booking.roomNumber);
+        const total = allRooms.filter(room => roomNumbers.includes(room.number))
+            .reduce((total, room) => {
+                total += room.costPerNight;
+                return total;
+            }, 0)
+        
+        return Number(total.toFixed(2));
+    }
+    
 }
 
 export default Customer;
