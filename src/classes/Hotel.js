bb9f8e4fc1c3c8a82f2a2a0812c8c71d47fb0136
Anna Spitz <aspitzemail@gmail.com>
2022-08-04 09:48:08 -0500

    Add WIP Hotel class

---
+++ src/classes/Hotel.js
@@ -0,0 +1,11 @@
+class Hotel {
+    constructor(allHotelData) {
+        this.allRooms = allHotelData.allRooms;
+        this.allBookings = allHotelData.allBookings;
+        this.allCustomers = allHotelData.allCustomers;
+    }
+
+    
+}
+
+export default Hotel;
