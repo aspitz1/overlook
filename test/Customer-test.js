bc92b2a8a30ac243d9786c1cff8ab3d8444a7214
Anna Spitz <aspitzemail@gmail.com>
2022-08-03 20:09:26 -0500

    Refactore Customer test

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -10,63 +10,44 @@ describe('Customer', () => {
         expect(Customer).to.be.a('function');
     });
 
-    it.skip('Should be an instence of Customer', () => {
+    it('Should be an instence of Customer', () => {
         const customer = new Customer(customers[1]);
-        expect(customer).to.be.an.instenceof(Customer);
+        expect(customer).to.be.an.instanceOf(Customer);
     });
 
-    it.skip('Should have an ID', () => {
+    it('Should have an ID', () => {
         const customer = new Customer(customers[1]);
         expect(customer.id).to.equal(12);
     });
 
-    it.skip('Should have a name', () => {
+    it('Should have a name', () => {
         const customer = new Customer(customers[0]);
         expect(customer.name).to.equal('Faustino Quitzon');
     });
 
-    it.skip('Should have a log in name', () => {
+    it('Should have a login name', () => {
         const customer = new Customer(customers[1]);
-        expect(customer.logInName).to.equal('customer12');
+        expect(customer.loginName).to.equal('customer12');
     });
 
-    it.skip('Should have a password', () => {
+    it('Should have a password', () => {
         const customer = new Customer(customers[1]);
         expect(customer.password).to.equal('overlook2021');
     });
 
-    it.skip('Should have future bookings', () => {
+    it('Should have future bookings', () => {
         const customer = new Customer(customers[0]);
         expect(customer.futureBookings).to.deep.equal([bookings[7]]);
     });
 
-    it.skip('Should have past bookings', () => {
+    it('Should have past bookings', () => {
         const customer = new Customer(customers[1]);
         expect(customer.pastBookings).to.deep.equal([bookings[2], bookings[4]]);
     });
 
-    it.skip('Should move bookings to past bookings', () => {
+    it('Should keep track of total spent on rooms', () => {
         const customer = new Customer(customers[0]);
-        expect(customer.pastBookings).to.deep.equal([bookings[0], bookings[1], bookings[3], bookings[7]]);
-    });
-
-    it.skip('Should make new bookings', () => {
-        const customer = new Customer(customers[2]);
-        expect(customer.futureBookings).to.deep.equal([bookings[6]]);
-        customer.bookRoom(rooms[5]);
-        expect(customer.futureBookings).to.deep.equal([bookings[6], rooms[5]]);
-    });
-
-    it.skip('Should cancel bookings', () => {
-        const customer = new Customer(customers[0]);
-        expect(customer.futureBookings).to.deep.equal([bookings[7]]);
-        customer.cancelBooking(bookings[7]);
-        expect(customer.futureBookings).to.equal([]);
-    });
-
-    it.skip('Should keep track of total spent on rooms', () => {
-        const customer = new Customer(customers[0]);
-        const totalSpent = customer.totalSpent(rooms);
+        const totalSpent = customer.returnTotalSpent(rooms);
         expect(totalSpent).to.equal(776.77);
     });
 
