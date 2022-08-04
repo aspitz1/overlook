de19aeacd85d61466c93548ccbf13004436b4d1c
Anna Spitz <aspitzemail@gmail.com>
2022-08-04 16:16:03 -0500

    Add tested Hotel class

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -2,9 +2,27 @@ class Hotel {
     constructor(allHotelData) {
         this.allRooms = allHotelData.allRooms;
         this.allBookings = allHotelData.allBookings;
-        this.allCustomers = allHotelData.allCustomers;
     }
 
+    getAvailibleAndUnavailibleRooms(date) {
+        const unavailibleRoomNums = this.allBookings.filter(booking => booking.date === date)
+            .map(booking => booking.roomNumber);
+       
+        return {availibleRooms: this.allRooms.filter(room => !unavailibleRoomNums.includes(room.number)),
+            unavailibleRooms: this.allRooms.filter(room => unavailibleRoomNums.includes(room.number))};
+    }
+
+    makeNewRoomID() {
+        return this.allBookings[this.allBookings.length - 1]
+    }
+
+    makeBookingObj(bookingData) {
+        return {
+            userID: bookingData.customer.id,
+            date: bookingData.date,
+            roomNumber: bookingData.room.number
+        }
+    }
     
 }
 
