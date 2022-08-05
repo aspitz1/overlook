8aa7d83a6c426bee750c2f88f6670344e83ec24d
Anna Spitz <aspitzemail@gmail.com>
2022-08-05 12:52:19 -0500

    Add working login page

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,9 @@
 import './images/favicon.png';
 import './css/style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 import Customer from './classes/Customer';
 import Manager from './classes/Manager';
@@ -19,12 +23,20 @@ let hotel;
 
 const hideOn = (elements) => elements.forEach(element => element.classList.add('hidden'));
 const hideOff = (elements) => elements.forEach(element => element.classList.remove('hidden'));
+const setError = (element) => {
+    element.setAttribute('style', 'color: red');
+    element.setAttribute('role', 'alert');
+    element.setAttribute('tabindex', -1);
+    element.focus();
+}
 
 const loginAsManager = () => {
     Promise.all([getFetch('customers'), getFetch('rooms'), getFetch('bookings')])
         .then(data => {
             manager = new Manager({customers: data[0].customers, allBookings: data[2].bookings});
             hotel = new Hotel({allRooms: data[1].rooms, allBookings: data[2].bookings});
+            hideOff([dashboardSectionManager]);
+            hideOn([loginSection]);
         })
         .catch(console.error);
 
@@ -34,18 +46,11 @@ const loginAsCustomer = (loginNum) => {
     Promise.all([getFetch(`customers/${loginNum}`), getFetch('rooms'), getFetch('bookings')])
         .then(data => {
             customer = new Customer({customer: data[0], allBookings: data[2].bookings});
-            hotel = new Hotel({allRooms: data[1].rooms, allBookings: data[2].bookings});   
+            hotel = new Hotel({allRooms: data[1].rooms, allBookings: data[2].bookings});
+            hideOff([dashboardSectionCustomer]);
+            hideOn([loginSection]);  
         })
         .catch(console.error);
-
-}
-
-const startApp = (loginData) => {
-    if (loginData.login === 'manager' && loginData.password === 'overlook2021') {
-        loginAsManager();
-    } else if (loginData.login.includes('customer') && loginData.password === 'overlook2021') {
-        loginAsCustomer(loginData.login.replace('customer', ''));
-    }
 
 }
 
@@ -53,15 +58,15 @@ window.addEventListener('load', () => {
     hideOff([loginSection]);
     loginSection.innerHTML = (`
         <h1 class="login-heading">Welcome to Overlook</h1>
-        <p class="login-description">Please sign in.</p>
+        <p class="login-description" id="loginDescription">Please sign in.</p>
         <form>
             <div class="login-name-wrapper">
                 <label class="hidden" for="login-name">Login Name: </label>
-                <input class="login-name-input" id="loginName" type="text" name="login-name" placeholder="Login Name" required>
+                <input class="login-name-input" id="loginName" type="text" name="login-name" placeholder="Login Name">
             </div>
             <div class="password-wrapper">
                 <label class="hidden" for="password">Password: </label>
-                <input class="password-input" id="password" type="password" name="password" placeholder="Password" required>
+                <input class="password-input" id="password" type="password" name="password" placeholder="Password">
             </div>
             <input class="login-submit-btn" id="loginSubmitBtn" type="submit" value="Login"> 
         <form>
@@ -73,8 +78,18 @@ loginSection.addEventListener('click', (event) => {
         event.preventDefault();
         const loginName = document.querySelector('#loginName').value;
         const password = document.querySelector('#password').value;
-        if (loginName.length || password.length) {
-            // make login-description give info on missing login/password. remember to focus
+        const loginDescription = document.querySelector('#loginDescription');
+        if (!loginName.length || !password.length) {
+            loginDescription.innerHTML = '<i class="fa-solid fa-x"></i> Please enter your login name and password.';
+            setError(loginDescription);
+        } else if (password === 'overlook2021' && loginName === 'manager') {
+            loginAsManager();
+        } else if (password === 'overlook2021' && loginName.includes('customer') && loginName.replace('customer', '') <= 50) {
+            loginAsCustomer(loginName.replace('customer', ''));
+        } else {
+            loginDescription.innerHTML = '<i class="fa-solid fa-x"></i> Invalid login credentials.';
+            setError(loginDescription);
         }
     }
+
 });
